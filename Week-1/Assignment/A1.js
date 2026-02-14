8567046416640644daffdaa3dac316d78ac95358
Sushant <mailtosk777@gmail.com>
2026-02-14 13:20:10 +0530

    week 1 complete

---
+++ Week-1/Assignment/A1.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto');
+
+
+function findHashWithPrefix(prefix) {
+    let input = 0;
+    while (true) {
+        // A2
+        let inputStr = "100xdevs"+input.toString();
+
+       //A1
+       //  let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash };
+        }
+        input++;
+    }
+}
+
+const result = findHashWithPrefix('00000');
+console.log(`Input: ${result.input}`);
+console.log(`Hash: ${result.hash}`);
